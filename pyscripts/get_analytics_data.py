c9b8e810d73026e70c61b470407d767ac60082b5
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-10-22 22:18:28 +0800

    update

--- pyscripts/get_analytics_data.py
+++ pyscripts/get_analytics_data.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-import itertools
 
 property_id = "G-VN909T72NS"
 starting_date = "8daysAgo"
@@ -21,32 +20,32 @@ client = BetaAnalyticsDataClient()
 request_api = RunReportRequest(
     property=f"properties/{property_id}",
     dimensions=[
-        Dimension(name="landingPagePlusQueryString")
-        ],
-        metrics=[
-            Metric(name="sessions")
-        ],
-        date_ranges=[DateRange(start_date=starting_date, end_date=ending_date)],
-    )
+        Dimension(name="landingPagePath")
+    ],
+    metrics=[
+        Metric(name="sessions")
+    ],
+    date_ranges=[DateRange(start_date=starting_date, end_date=ending_date)],
+)
+
 response = client.run_report(request_api)
 
 def query_data(api_response):
     dimension_headers = [header.name for header in api_response.dimension_headers]
     metric_headers = [header.name for header in api_response.metric_headers]
-    dimensions = []
-    metrics = []
-    for i in range(len(dimension_headers)):
-        dimensions.append([row.dimension_values[i].value for row in api_response.rows])
-    dimensions
-    for i in range(len(metric_headers)):
-        metrics.append([row.metric_values[i].value for row in api_response.rows])
-    headers = dimension_headers, metric_headers
-    headers = list(itertools.chain.from_iterable(headers))   
-    data = dimensions, metrics
-    data = list(itertools.chain.from_iterable(data))
-    df = pd.DataFrame(data)
-    df = df.transpose()
-    df.columns = headers
+    
+    headers = [item for sublist in zip(dimension_headers, metric_headers) for item in sublist]
+    
+    data = []
+    for row in api_response.rows:
+        row_data = []
+        for i in range(len(dimension_headers)):
+            row_data.append(row.dimension_values[i].value)
+        for i in range(len(metric_headers)):
+            row_data.append(row.metric_values[i].value)
+        data.append(row_data)
+    
+    df = pd.DataFrame(data, columns=headers)
     return df
 
 final_data = query_data(response)
