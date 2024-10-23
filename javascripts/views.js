766d5b699073c3e7d412ee1dedfb6490436f646d
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-10-23 21:03:15 +0800

    Update views data

--- javascripts/views.js
+++ javascripts/views.js
@@ -1,7 +1,7 @@
 // const data
-const ViewsHome = "18";
-const ViewsResearch = "11";
-const ViewsEducation = "5";
+const ViewsHome = "26";
+const ViewsResearch = "14";
+const ViewsEducation = "9";
 const lastUpdated = "October 23, 2024";
 
 // function
@@ -10,7 +10,7 @@ function displayLastUpdatedData() {
   const dateElement = document.getElementById("lastUpdatedDate");
   const ResearchElement = document.getElementById("viewsResearch");
   const EducationElement = document.getElementById("viewsEducation");
-  const iconViews = `<img src="files/images/logo/views.png" width="9.5" height="9.5" style="vertical-align: -1px;margin-right:1px;">`;
+  const iconViews = `<img src="files/images/logo/views.webp" width="9.5" height="9.5" style="vertical-align: -1px;margin-right:1px;">`;
   const iconUpdate = `<img src="files/images/logo/update.webp" width="9.5" height="9.5" style="vertical-align: -1px;margin-right:1.2px;">`;
   const analyticsLink = `<a href="https://www.simpleanalytics.com/?referral=qofoc" target="_blank" class="black-link">Simple Analytics</a>`;  
   
