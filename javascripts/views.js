72f7a05ee7656591908eeffe7891ee39998960b7
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-10-23 18:01:34 +0800

    Update

--- javascripts/views.js
+++ javascripts/views.js
@@ -1,7 +1,7 @@
 // const data
 const ViewsHome = "19";
 const ViewsResearch = "9";
-const ViewsEducation = "3";
+const ViewsEducation = "5";
 const lastUpdated = "October 23, 2024";
 
 // function
@@ -12,8 +12,7 @@ function displayLastUpdatedData() {
   const EducationElement = document.getElementById("viewsEducation");
   const iconViews = `<img src="files/images/logo/views.png" width="9.5" height="9.5" style="vertical-align: -1px;margin-right:1px;">`;
   const iconUpdate = `<img src="files/images/logo/update.webp" width="9.5" height="9.5" style="vertical-align: -1px;margin-right:1.2px;">`;
-  const analyticsLink = `<a href="https://analytics.google.com" target="_blank" class="black-link">Google Analytics</a>`;
-  
+  const analyticsLink = `<a href="https://analytics.google.com/" target="_blank" class="black-link">Google Analytics</a>`;  
   
   if (dateElement) {
     dateElement.innerHTML = `${iconUpdate} Last content updated: ${lastUpdated}`;
