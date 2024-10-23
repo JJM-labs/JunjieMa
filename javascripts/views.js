6af998a250875a7ad7e0e366e0add4cb981ecea6
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-10-23 21:18:36 +0800

    Update page views data

--- javascripts/views.js
+++ javascripts/views.js
@@ -10,12 +10,12 @@ function displayLastUpdatedData() {
   const dateElement = document.getElementById("lastUpdatedDate");
   const ResearchElement = document.getElementById("viewsResearch");
   const EducationElement = document.getElementById("viewsEducation");
-  const iconViews = `<img src="files/images/logo/views.webp" width="9.5" height="9.5" style="vertical-align: -1px;margin-right:1px;">`;
-  const iconUpdate = `<img src="files/images/logo/update.webp" width="9.5" height="9.5" style="vertical-align: -1px;margin-right:1.2px;">`;
-  const analyticsLink = `<a href="https://www.simpleanalytics.com/?referral=qofoc" target="_blank" class="black-link">Simple Analytics</a>`;  
+  const iconViews = `<img src="files/images/logo/views.webp" width="9.5" height="9.5" style="vertical-align:-1px;margin-right:1px;">`;
+  const iconUpdate = `<img src="files/images/logo/update.webp" width="9.5" height="9.5" style="vertical-align:-1px;margin-right:1.2px;">`;
+  const analyticsLink = `<a href="https://www.simpleanalytics.com/?referral=qofoc" target="_blank">Simple Analytics</a>`;  
   
   if (dateElement) {
-    dateElement.innerHTML = `${iconUpdate} Last content updated: ${lastUpdated}`;
+    dateElement.innerHTML = `${iconUpdate} Last Updated Time: ${lastUpdated}`;
   }
   if (HomeElement) {
     HomeElement.innerHTML = `${iconViews} Views: ${ViewsHome} (by ${analyticsLink})`;
