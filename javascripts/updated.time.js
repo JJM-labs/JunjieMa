ca41e133dd5e763e68278567c0939a708793590e
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-10-23 13:25:35 +0800

    Update

--- javascripts/updated.time.js
+++
@@ -1,9 +0,0 @@
-const lastUpdated = "October 19, 2024";
-function displayLastUpdatedDate() {
-    const dateElement = document.getElementById("lastUpdatedDate");
-    if (dateElement) {
-        const iconHTML = `<img src="files/images/logo/update.webp" width="9.5" height="9.5" style="vertical-align: -1px;">`;
-        dateElement.innerHTML = `${iconHTML} Last content updated: ${lastUpdated}`;
-    }
-}
-window.onload = displayLastUpdatedDate;
---
+++ javascripts/views.js
@@ -0,0 +1,32 @@
+// const data
+const ViewsHome = "19";
+const ViewsResearch = "9";
+const ViewsEducation = "3";
+const lastUpdated = "October 23, 2024";
+
+// function
+function displayLastUpdatedData() {
+  const HomeElement = document.getElementById("viewsHome");
+  const dateElement = document.getElementById("lastUpdatedDate");
+  const ResearchElement = document.getElementById("viewsResearch");
+  const EducationElement = document.getElementById("viewsEducation");
+  const iconViews = `<img src="files/images/logo/views.png" width="9.5" height="9.5" style="vertical-align: -1px;margin-right:1px;">`;
+  const iconUpdate = `<img src="files/images/logo/update.webp" width="9.5" height="9.5" style="vertical-align: -1px;margin-right:1.2px;">`;
+  const analyticsLink = `<a href="https://analytics.google.com" target="_blank" class="black-link">Google Analytics</a>`;
+  
+  
+  if (dateElement) {
+    dateElement.innerHTML = `${iconUpdate} Last content updated: ${lastUpdated}`;
+  }
+  if (HomeElement) {
+    HomeElement.innerHTML = `${iconViews} Views: ${ViewsHome} (by ${analyticsLink})`;
+  }
+  if (ResearchElement) {
+    ResearchElement.innerHTML = `${iconViews} Views: ${ViewsResearch} (by ${analyticsLink})`;
+  }
+  if (EducationElement) {
+    EducationElement.innerHTML = `${iconViews} Views: ${ViewsEducation} (by ${analyticsLink})`;
+  }
+}
+
+window.onload = displayLastUpdatedData;
