59741022d8868a096f91ac7e400ff1b7c45a3bfb
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-11-03 17:53:21 +0800

    Update pageviews data

--- javascripts/pageviews.js
+++ javascripts/pageviews.js
@@ -1,31 +1,31 @@
 // const data
-const ViewsHome = "26";
-const ViewsResearch = "15";
-const ViewsEducation = "9";
-const lastUpdated = "October 24, 2024";
+const viewsHome="38";
+const viewsResearch="32";
+const viewsEducation="20";
+const lastUpdated="November 3, 2024";
 
 // function
 function displayLastUpdatedData() {
-  const HomeElement = document.getElementById("viewsHome");
-  const dateElement = document.getElementById("lastUpdatedDate");
-  const ResearchElement = document.getElementById("viewsResearch");
-  const EducationElement = document.getElementById("viewsEducation");
-  const iconViews = `<img src="files/images/logo/views.webp" width="9.5" height="9.5" style="vertical-align:-1px;margin-right:1px;">`;
-  const iconUpdate = `<img src="files/images/logo/update.webp" width="9.5" height="9.5" style="vertical-align:-1px;margin-right:1.2px;">`;
-  const analyticsLink = `<a href="https://analytics.google.com" target="_blank">Google Analytics</a>`;  
+  const HomeElement=document.getElementById("viewsHome");
+  const dateElement=document.getElementById("lastUpdatedDate");
+  const ResearchElement=document.getElementById("viewsResearch");
+  const EducationElement=document.getElementById("viewsEducation");
+  const iconviews=`<img src="files/images/logo/views.webp" width="9.5" height="9.5" style="vertical-align:-1px;margin-right:1px;">`;
+  const iconUpdate=`<img src="files/images/logo/update.webp" width="9.5" height="9.5" style="vertical-align:-1px;margin-right:1.2px;">`;
+  const analyticsLink=`<a href="https://analytics.google.com" target="_blank">Google Analytics</a>`;  
   
   if (dateElement) {
-    dateElement.innerHTML = `${iconUpdate} Last updated time: ${lastUpdated}`;
+    dateElement.innerHTML=`${iconUpdate} Last updated time: ${lastUpdated}`;
   }
   if (HomeElement) {
-    HomeElement.innerHTML = `${iconViews} Pageviews: ${ViewsHome} (by ${analyticsLink})`;
+    HomeElement.innerHTML=`${iconviews} Pageviews: ${viewsHome} (by ${analyticsLink})`;
   }
   if (ResearchElement) {
-    ResearchElement.innerHTML = `${iconViews} Pageviews: ${ViewsResearch} (by ${analyticsLink})`;
+    ResearchElement.innerHTML=`${iconviews} Pageviews: ${viewsResearch} (by ${analyticsLink})`;
   }
   if (EducationElement) {
-    EducationElement.innerHTML = `${iconViews} Pageviews: ${ViewsEducation} (by ${analyticsLink})`;
+    EducationElement.innerHTML=`${iconviews} Pageviews: ${viewsEducation} (by ${analyticsLink})`;
   }
 }
 
-window.onload = displayLastUpdatedData;
+window.onload=displayLastUpdatedData;
