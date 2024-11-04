0e9a6cde325df9eac0c961441b6321f21c8a5c40
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-11-04 14:47:57 +0800

    Update page layout

---
+++ javascripts/scroll.button.js
@@ -0,0 +1,84 @@
+// var
+var scrollToTopButton = document.getElementById("scrollToTop");
+var firstHeading = document.querySelector("section h2:first-of-type");
+var scrollToSelector = document.getElementById('scrollToSection');
+
+// Viewport
+function isOutOfViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= window.innerHeight || 
+        rect.bottom < 0 ||                 
+        rect.right < 0 ||                  
+        rect.left >= window.innerWidth     
+    );
+}
+
+// Button Visibility
+function buttonVisibility() {
+    if (isOutOfViewport(firstHeading)) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+}
+
+// scrollToTop
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+// scrollToSection
+function scrollToSection(sectionId) {
+  var section = document.getElementById(sectionId);
+  if (section) {
+    var scrollPosition = section.offsetTop - 150;
+    if (scrollPosition < 0) {
+      scrollPosition = 0;
+    }
+    window.scrollTo({
+      top: scrollPosition,
+      ehavior: 'smooth'
+    });
+  }
+}
+
+// Top Button Position
+function topButtonPosition() {
+  const section = document.querySelector('section');
+  if (section) {
+    const sectionRect = section.getBoundingClientRect();
+    let buttonRight = window.innerWidth - (sectionRect.right + 200);
+    buttonRight = Math.max(buttonRight, 0);
+    scrollToTopButton.style.right = `${buttonRight}px`;
+  }
+}
+
+// Section Button Position
+function sectionButtonPosition() {
+  const section = document.querySelector('section');
+  if (section) {
+    const sectionRect = section.getBoundingClientRect();
+    let buttonRight = window.innerWidth - (sectionRect.right + 190);
+    buttonRight = Math.max(buttonRight, 0);
+    scrollToSelector.style.right = `${buttonRight}px`;
+  }
+}
+
+window.addEventListener("scroll", buttonVisibility);
+scrollToTopButton.addEventListener("click", scrollToTop);
+window.addEventListener('resize', () => {
+  topButtonPosition();
+  sectionButtonPosition();
+});
+document.addEventListener('DOMContentLoaded', () => {
+  topButtonPosition();
+  sectionButtonPosition();
+  buttonVisibility(); 
+});
+
+buttonVisibility();
+
