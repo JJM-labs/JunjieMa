7b5213a44724b86abecf1e901b3d80605cbd1ae1
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-11-05 15:23:41 +0800

    Update page layout

--- javascripts/modal.js
+++ javascripts/modal.js
@@ -12,19 +12,30 @@ window.onclick = function(event) {
       }
     });
 };
+
+function showModal(modalId) {
+  document.getElementById(modalId).style.display = 'flex';
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = 'none';
+}
+
 function showModalWithFile(modalId, outTextId, fileName) {
+  const uniqueId = outTextId.split('-')[1];
+  const loadingId = 'loading-' + uniqueId;
+  const loading = document.getElementById(loadingId);
   showModal(modalId);
-  document.getElementById('loading').style.display = 'flex';
+  loading.style.display = 'flex';
   fetch(fileName)
     .then(response => response.text())
     .then(data => {
+      loading.style.display = 'none'; 
       document.getElementById(outTextId).innerHTML = data;
-      document.getElementById('loading').style.display = 'none';
     })
     .catch(error => {
       console.error('Error fetching file:', error);
+      loading.style.display = 'none';
       document.getElementById(outTextId).innerText = 'Error loading file.';
-      document.getElementById('loading').style.display = 'none';
     });
 }
-
