8bb078af1636ea365aee9fe215154093736c9602
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-11-05 14:08:16 +0800

    Update page layout

--- javascripts/modal.js
+++ javascripts/modal.js
@@ -1,26 +1,30 @@
 function showModal(modalId) {
-    document.getElementById(modalId).style.display = 'flex';
+  document.getElementById(modalId).style.display = 'flex';
 }
 function closeModal(modalId) {
-    document.getElementById(modalId).style.display = 'none';
+  document.getElementById(modalId).style.display = 'none';
 }
 window.onclick = function(event) {
     var modals = document.querySelectorAll('.modal');
     modals.forEach(modal => {
-        if (event.target === modal) {
-            closeModal(modal.id); 
-        }
+      if (event.target === modal) {
+        closeModal(modal.id); 
+      }
     });
 };
 function showModalWithFile(modalId, outTextId, fileName) {
   showModal(modalId);
+  document.getElementById('loading').style.display = 'flex';
   fetch(fileName)
-      .then(response => response.text())
-      .then(data => {
-          document.getElementById(outTextId).innerHTML = data;
-      })
-      .catch(error => {
-          console.error('Error fetching file:', error);
-          document.getElementById(outTextId).innerText = 'Error loading file.';
-      });
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(outTextId).innerHTML = data;
+      document.getElementById('loading').style.display = 'none';
+    })
+    .catch(error => {
+      console.error('Error fetching file:', error);
+      document.getElementById(outTextId).innerText = 'Error loading file.';
+      document.getElementById('loading').style.display = 'none';
+    });
 }
+
