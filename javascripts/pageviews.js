9bf8dbba632286ea532f851095a053c3488e954e
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-11-08 21:07:43 +0800

    Update pageviews

--- javascripts/pageviews.js
+++ javascripts/pageviews.js
@@ -1,8 +1,8 @@
 // const data
-const viewsHome="38";
-const viewsResearch="32";
-const viewsEducation="20";
-const lastUpdated="November 3, 2024";
+const viewsHome="54";
+const viewsResearch="46";
+const viewsEducation="35";
+const lastUpdated="November 8, 2024";
 
 // function
 function displayLastUpdatedData() {
@@ -10,7 +10,7 @@ function displayLastUpdatedData() {
   const dateElement=document.getElementById("lastUpdatedDate");
   const ResearchElement=document.getElementById("viewsResearch");
   const EducationElement=document.getElementById("viewsEducation");
-  const iconviews=`<img src="files/images/logo/views.webp" width="9.5" height="9.5" style="vertical-align:-1px;margin-right:1px;">`;
+  const iconViews=`<img src="files/images/logo/views.webp" width="9.5" height="9.5" style="vertical-align:-1px;margin-right:1px;">`;
   const iconUpdate=`<img src="files/images/logo/update.webp" width="9.5" height="9.5" style="vertical-align:-1px;margin-right:1.2px;">`;
   const analyticsLink=`<a href="https://analytics.google.com" target="_blank">Google Analytics</a>`;  
   
@@ -18,13 +18,13 @@ function displayLastUpdatedData() {
     dateElement.innerHTML=`${iconUpdate} Last updated time: ${lastUpdated}`;
   }
   if (HomeElement) {
-    HomeElement.innerHTML=`${iconviews} Pageviews: ${viewsHome} (by ${analyticsLink})`;
+    HomeElement.innerHTML=`${iconViews} Pageviews: ${viewsHome} (by ${analyticsLink})`;
   }
   if (ResearchElement) {
-    ResearchElement.innerHTML=`${iconviews} Pageviews: ${viewsResearch} (by ${analyticsLink})`;
+    ResearchElement.innerHTML=`${iconViews} Pageviews: ${viewsResearch} (by ${analyticsLink})`;
   }
   if (EducationElement) {
-    EducationElement.innerHTML=`${iconviews} Pageviews: ${viewsEducation} (by ${analyticsLink})`;
+    EducationElement.innerHTML=`${iconViews} Pageviews: ${viewsEducation} (by ${analyticsLink})`;
   }
 }
 
