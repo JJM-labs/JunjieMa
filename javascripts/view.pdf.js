15399965b7addaa5e5237b7a26eaada772ff0e58
JunJie Ma <161120359+JJM-labs@users.noreply.github.com>
2024-12-01 08:57:13 +0800

    Update page layout

--- javascripts/view.pdf.js
+++ javascripts/view.pdf.js
@@ -1,11 +1,29 @@
-function viewPdf(pdfUrl, frameId) {
-    var iframe = document.getElementById(frameId);
-    if (iframe.style.display === 'none') {
-        iframe.src = pdfUrl;
-        iframe.style.display = 'block';
-    } else {
-        iframe.style.display = 'none';
-        iframe.src = '';
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.9.359/pdf.worker.min.js';
+async function renderPDF(url) {
+  document.getElementById('loadingIndicator').style.display = 'block';
+  const loadingTask = pdfjsLib.getDocument(url);
+  const pdf = await loadingTask.promise;
+  const numPages = pdf.numPages;
+  for (let i = 1; i <= numPages; i++) {
+    const page = await pdf.getPage(i);
+    const scale = 1.5;
+    const viewport = page.getViewport({ scale: scale });
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+    const renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+    await page.render(renderContext).promise;
+    const pageContainer = document.createElement("div");
+    pageContainer.className = "pdf-page";
+    pageContainer.appendChild(canvas);
+    document.getElementById('pdf-viewer').appendChild(pageContainer);
+    if (i === numPages) {
+      document.getElementById('loadingIndicator').style.display = 'none';
     }
-    return false;
+  }
 }
+renderPDF('files/content/cv/CV.pdf');
